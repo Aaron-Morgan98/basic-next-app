fc0f60738ec3fb8759d0bfe5dd18b94ebcdda06c
Aaron-Morgan98 <aaron.morgan742@gmail.com>
2024-07-09 11:10:45 +0100

    setting up tests

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,4 +1,10 @@
-import Home from "@/app/[locale]/page"
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import Home from "../src/app/[locale]/page";
+import "@testing-library/jest-dom";
 
-
+describe("Home", () => {
+  it("renders without crashing", () => {
+    render(<Home />);
+    expect(screen.getByRole("button", { name: /more info/i })).toBeInTheDocument();
+  });
+});
