020f87aa3ca788f2f4b2391c5e6b4de97baa38cc
Aaron Morgan <morgana4@emea.cshare.net>
2024-08-16 12:56:44 +0100

    housekeeping

--- src/app/[locale]/moreInfo/[id]/page.tsx
+++ src/app/[locale]/moreInfo/[id]/page.tsx
@@ -16,49 +16,12 @@ export default async function MoreInfo({
     id:string;
   };
 }) {
-  // const t = useTranslations("MoreInfo");
-  // const router = useRouter();
-  
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const id = urlParams.get("id");
 
 
   const data = await getDataById(id);
 
   console.log(data);
 
-  // const [data, setData] = useState<any | null>(null);
-  // const [id, setId] = useState<string | null>(null);
-  // const [name, setName] =useState<string | null>(null);
-
-  //handle grabbing the info from the main page and then setting them in the respective states
-  // useEffect(() => {
-  //   const queryParams = new URLSearchParams(window.location.search);
-  //   const id = queryParams.get("id");
-  //   const name = queryParams.get("name");
-  //   const data = queryParams.get("data");
-
-  //   //set and decode info sent over from main page
-  //   if (id) {
-  //     setId(id);
-  //   }
-
-  //   if(name){
-  //       setName(decodeURIComponent(name));
-  //   }
-
-  //   if (data) {
-  //     try {
-  //       // data needs extra step of being parsed
-  //       const decodedData = decodeURIComponent(data);
-  //       const parsedData = JSON.parse(decodedData);
-  //       setData(parsedData);
-  //     } catch (err) {
-  //       console.error('Failed to parse JSON data:', err);
-  //       setData({});
-  //     }
-  //   }
-  // }, []);
   // turn the JSON data into a more readable format by splitting into key value pairs
   // const renderDataList = (data: Record<string, any>) => {
   //   return (
@@ -78,69 +41,5 @@ export default async function MoreInfo({
 
   return (
         <MoreInfoCards id={data.id} name={data.name} data={data.data} />
-
-    // <Container>
-    //   <Box sx={{ textAlign: 'center', mb: 4, mt: 4 }}>
-    //     <Typography variant="h4">
-    //       {t("MORE_INFORMATION")}
-    //     </Typography>
-    //   </Box>
-      
-    //     <Grid container spacing={3} justifyContent={"center"}>
-    //       <Grid item xs={12} md={6}>
-    //         <Card>
-    //           <CardContent>
-    //             <Typography variant="h6" gutterBottom>
-    //               ID
-    //             </Typography>
-    //             <Typography variant="body1">
-    //               {id || 'No ID available'}
-    //             </Typography>
-    //           </CardContent>
-    //         </Card>
-    //       </Grid>
-    //       <Grid item xs={12} md={6}>
-    //         <Card>
-    //           <CardContent>
-    //             <Typography variant="h6" gutterBottom>
-    //               {t("NAME")}
-    //             </Typography>
-    //             <Typography variant="body1">
-    //               {name || 'No name available'}
-    //             </Typography>
-    //           </CardContent>
-    //         </Card>
-    //       </Grid>
-    //       <Grid item xs={12} md={6}>
-    //         <Card>
-    //           <CardContent
-    //             sx={{
-    //               display: 'flex',
-    //               flexDirection: 'column',
-    //               alignItems: 'center',
-    //               justifyContent: 'center',
-    //             }}
-    //           >
-    //             <Typography variant="h6" gutterBottom align="center">
-    //               {t("DETAILS")}
-    //             </Typography>
-    //             {data ? (
-    //               <Box sx={{ textAlign: 'center' }}>
-    //                 {renderDataList(data)}
-    //               </Box>
-    //             ) : (
-    //               <Typography>No data available</Typography>
-    //             )}
-    //           </CardContent>
-    //         </Card>
-    //       </Grid>
-    //     </Grid>
-        // <Stack direction="row" mt={3} justifyContent="center">
-        //   <Button variant="outlined" onClick={handleBackButtonClick}>
-        //     {t("BACK_BUTTON")}
-        //   </Button>
-        // </Stack> 
-      
-    // </Container>
   );
 }
